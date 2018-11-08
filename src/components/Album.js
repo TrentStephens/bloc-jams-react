8935365239381ac691c28bb488087fd78a3a72fb
Trent Stephens <trentstephens.advertising@gmail.com>
2018-11-07 21:19:07 -0500

    Bloc React Jams Audio Playback Checkpoint Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,10 +10,40 @@ class Album extends Component {
 		return album.slug === this.props.match.params.slug
 	});
 
-	this.state = {
-		album: album
-	};
+			this.state = {
+				album: album,
+				currentSong: album.songs[0],
+				isPlaying: false
+		};
+
+	this.audioElement = document.createElement('audio');
+	this.audioElement.src = album.songs[0].audioSrc;
 	}
+
+	play() {
+		this.audioElement.play();
+		this.setState({ isPlaying: true});
+	}
+
+	pause() {
+		this.audioElement.pause();
+		this.setState({ isPlaying: false});
+	}
+
+	setSong(song) {
+		this.audioElement.src = song.audioSrc;
+		this.setState({ currentSong: song});
+	}
+
+	handleSongClick(song){
+		const isSameSong = this.state.currentSong === song;
+		if (this.state.isPlaying && isSameSong){
+			this.pause();
+		} else{
+			if (!isSameSong) {this.setSong(song);}
+			this.play();
+		}
+}
 
 	render(){
 		return (
@@ -36,7 +66,9 @@ class Album extends Component {
 
 				<tbody class='songList'>
 					{this.state.album.songs.map ( ( song, index) =>
-							<tr>{index + 1} - {song.title} - {song.duration} seconds</tr>
+							<tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+							{index + 1} - {song.title} - {song.duration} seconds
+							</tr>
 						)
 					}
 				</tbody>
